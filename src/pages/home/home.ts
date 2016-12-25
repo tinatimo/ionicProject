495d2363b60433a842952635d9e0b1ef176b96fc
tinatimo <smallting@hotmail.co.uk>
2016-12-25 17:39:43 +0000

    create basic info javascript object

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,29 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+   inputName: string;
+   inputDOB: number;
+   inputGender: string;
+   inputNationality: string;
+   inputSchool: string;
+   inputCourse: string;
+   inputGPA: number;
   constructor(public navCtrl: NavController) {
-    
+
+  }
+  goToAboutYouPage(){
+    var info = {
+      "name": this.inputName,
+      "dob": this.inputDOB,
+      "gender": this.inputGender,
+      "nationality":this.inputNationality,
+      "current_school":this.inputSchool,
+      "current_major":this.inputCourse,
+      "current_gpa":this.inputGPA,
+    }
+
+    console.log(info);
+
   }
 
 }
