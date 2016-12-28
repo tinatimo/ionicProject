fc63b8fc743ef805aadd084bbb3af0104384b4d9
tinatimo <smallting@hotmail.co.uk>
2016-12-28 19:30:43 +0000

    form validation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController, Platform } from 'ionic-angular';
 import { File } from 'ionic-native';
+import {FormGroup,Validators, FormBuilder } from '@angular/forms';
+
 declare var cordova: any;
 
 @Component({
@@ -18,27 +20,47 @@ export class HomePage {
    inputGPA: number;
    platform: any;
    fs: any;
-  constructor(public navCtrl: NavController, platform: Platform) {
-    this.platform = platform
-  }
-  goToAboutYouPage(){
-    var info = {
-      "name": this.inputName,
-      "dob": this.inputDOB,
-      "gender": this.inputGender,
-      "nationality":this.inputNationality,
-      "current_school":this.inputSchool,
-      "current_major":this.inputCourse,
-      "current_gpa":this.inputGPA,
-    }
-    if (this.platform.is('core')) { //if this is desktop browser
-      localStorage.setItem('userInfo', JSON.stringify(info));
-    } else { 
-      this.platform.ready().then((readySource) => {
-        File.writeFile(cordova.file.dataDirectory, "userInfo.json", JSON.stringify(info), true)
-      })
+   myForm: FormGroup;
+  constructor(public navCtrl: NavController, platform: Platform,private formBuilder: FormBuilder) {
 
-    }
+    this.myForm = this.formBuilder.group({
+      inputName: ['', Validators.required],
+      inputDOB:['', Validators.required],
+      inputGender:['',Validators.required],
+      inputNationality:['',Validators.required],
+      inputSchool:['',Validators.required],
+      inputCourse:['',Validators.required],
+      inputGPA:['',Validators.compose([Validators.required, Validators.maxLength(2)])],
+    });
+
+    this.platform = platform;
   }
+
+  logForm(){
+    var info = {
+"name": this.inputName,
+"dob": this.inputDOB,
+"gender": this.inputGender,
+"nationality":this.inputNationality,
+"current_school":this.inputSchool,
+"current_major":this.inputCourse,
+"current_gpa":this.inputGPA,
+};
+
+console.log(this.myForm.value);
+console.log(info);
+
+if (this.platform.is('core')) { //if this is desktop browser
+  localStorage.setItem('userInfo', JSON.stringify(info));
+
+} else {
+  this.platform.ready().then((readySource) => {
+    File.writeFile(cordova.file.dataDirectory, "userInfo.json", JSON.stringify(info), true)
+  })
+
+};
+  }
+
+
 
 }
