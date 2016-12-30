e5f02c8afd23d705d957999879310363442c5adf
tinatimo <smallting@hotmail.co.uk>
2016-12-30 15:59:27 +0000

    add selected content to local storage

--- src/pages/aboutYou/aboutYou.ts
+++ src/pages/aboutYou/aboutYou.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import{File} from 'ionic-native';
+declare var cordova: any;
 
 @Component({
   selector: 'page-aboutYou',
@@ -8,6 +11,12 @@ import { NavController } from 'ionic-angular';
 })
 
 export class AboutYouPage {
+ if (this.platform.is('core')) {
+} else {
+  this.platform.ready().then((readySource) => {
+    File.writeFile(cordova.file.dataDirectory, "userInfo.json", JSON.stringify(this.userInfo), true)
+  });
+};
 
   constructor(public navCtrl: NavController) {
 
